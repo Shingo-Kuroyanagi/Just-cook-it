fdd55679cd869d0583720de9f27f9e886cd74b6e
EC2 Default User <ec2-user@ip-172-31-48-28.ec2.internal>
2020-11-26 10:01:59 +0000

    [Update]デフォルト修正

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,13 @@ ActiveRecord::Schema.define(version: 2020_11_16_084930) do
     t.integer "sex", default: 0, null: false
     t.integer "age", default: 0, null: false
     t.string "image_id", default: "", null: false
-    t.text "introduction", default: "", null: false
-    t.boolean "is_deleted", default: false, null: false
+    t.text "introduction", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
